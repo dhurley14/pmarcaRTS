1010e787a11d6e2b05739eca776fb3afba00bee7
Devin Hurley <snowmiser111@mac.com>
2014-11-25 23:52:43 -0500

    last minute commit

--- src/get_tweets.py
+++ src/get_tweets.py
@@ -9,32 +9,34 @@ def get_authentication():
 
     return a new instance of the Twython object.
     """
-    APP_KEY = 'xxxxxxx'
-    APP_SECRET = 'xxxxxxxxxxxx'
+    APP_KEY = 'xxx'
+    APP_SECRET = 'xxxxx'
     twitter = Twython(APP_KEY, APP_SECRET)
     return twitter
 
-def get_tweets(username='pmarca'):
+def get_tweets(username='pmarca', tweetID):
     """ Get a user's timeline
 
     :param username: (default pmarca)
+    :param tweetID: 
     """
     twitter = get_authentication()
     # to get all 3200 possible tweets, I must cycle
     # through, and change the max_id on each call to be the lowest
     # id , so that my next call gets all the tweets below that id,
     # and so on and so forth.
-    user_timeline = twitter.get_user_timeline(screen_name=username, count=200, max_id=536687397044772864)
+    user_timeline = twitter.get_user_timeline(screen_name=username, count=200, max_id=tweetID)
     return user_timeline
 
 def output_all_rts():
-    user_timeline = get_tweets()
+    user_timeline = get_tweets(tweetID)
+    
     rts = [user_timeline[i]['text'].encode('utf-8') for i in xrange(len(user_timeline)) 
         if "RT " in user_timeline[i]['text']]
-    print len(rts)
-    output_file = open("pmarca_rts.txt", "w+b")
+    #print len(rts)
+    output_file = open("pmarca_rts_append.txt", "a+b")
     for line in rts:
-        output_file.write(''.join(line)+"\n")
+        output_file.append(''.join(line)+"\n")
     output_file.close()
 
 if __name__ == '__main__':
