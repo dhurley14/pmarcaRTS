cde780ee102a7a27c7eedaf0ce9b38fe875047ec
Devin Hurley <snowmiser111@mac.com>
2014-11-27 19:43:34 -0500

    found a useful LDA library

---
+++ src/analyze_tweets.py
@@ -0,0 +1,6 @@
+""" Check out the gensim package for mapping topics to textual data.
+
+http://radimrehurek.com/gensim/wiki.html
+rather than using what Joseph Misit used here...
+http://josephmisiti.github.io/using-latent-dirichlet-allocation-to-categorize-my-twitter-feed/
+"""
