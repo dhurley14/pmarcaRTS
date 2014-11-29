c4faec9c64eb880da476e49fe791593e8c9eddc4
Devin Hurley <snowmiser111@mac.com>
2014-11-28 23:12:31 -0500

    added a link to LDA paper

--- src/analyze_tweets.py
+++ src/analyze_tweets.py
@@ -3,4 +3,7 @@
 http://radimrehurek.com/gensim/wiki.html
 rather than using what Joseph Misit used here...
 http://josephmisiti.github.io/using-latent-dirichlet-allocation-to-categorize-my-twitter-feed/
+Also use this to try and figure out what the LDA algo in gensim is actually 
+doing
+http://www.cs.princeton.edu/~blei/papers/BleiNgJordan2003.pdf
 """
