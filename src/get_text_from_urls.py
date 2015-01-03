ff41a13b4ef051019df21efb4e20d211cfb3c413
Devin Hurley <snowmiser111@mac.com>
2015-01-03 17:02:58 -0500

    extracting urls from tweets

---
+++ src/get_text_from_urls.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import requests
+import json
+
+""" This script will pass my list of URL's with articles to diffbot,
+which will then extract the body text and write that to a file I 
+will then download.  This script is a copy of the code I found on
+blog.mathandpencil.com/using-latent-dirichlet-allocation-to-categorize-my-twitter-feed/
+
+"""
+
+DIFF_BOT_TOKEN = 'xxx'
+
+def create_bulk_job(urls, name):
+    """ urls will be whitespace separated values
+    
+    """
+    apiUrl = 'http://api.diffbot.com/v3/article'
+    params = dict(token=DIFF_BOT_TOKEN, name=name, urls=urls, apiUrl=apiUrl)
+    response = requests.post('http://api.diffbot.com/v3/bulk',data=params)
+    moarData = json.loads(response.content)
+    print moarData['jobs'][0]['downloadJson']
+
+if __name__ == '__main__':
+    tweetDICTurls = ast.literal_eval(open("tweetdict.txt").read())
+    urls = " ".join([u.strip().replace('"','') for u in tweetDICTurls.keys()])
+    name = "HURLEY"
+    create_bulk_job(urls=urls,name=name)
---
+++ src/load_file_to_dict.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import ast
+""" useful for parsing the file with the mapped URLs to tweets
+as a python dict
+
+"""
+some_data = open(sys.argv[1]).read()
+result = ast.literal_eval(some_data)
+print result.keys()
