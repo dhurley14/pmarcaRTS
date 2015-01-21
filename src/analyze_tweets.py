c6a5d7b298940b63154a988ee756dabc29508cea
Devin Hurley <snowmiser111@mac.com>
2015-01-21 18:27:43 -0500

    organized code better

--- src/analyze_tweets.py
+++
@@ -1,9 +0,0 @@
-""" Check out the gensim package for mapping topics to textual data.
-
-http://radimrehurek.com/gensim/wiki.html
-rather than using what Joseph Misit used here...
-http://josephmisiti.github.io/using-latent-dirichlet-allocation-to-categorize-my-twitter-feed/
-Also use this to try and figure out what the LDA algo in gensim is actually 
-doing
-http://www.cs.princeton.edu/~blei/papers/BleiNgJordan2003.pdf
-"""
--- src/pract/doc_to_vec.py
+++
@@ -1,5 +0,0 @@
-import gensim, logging
-
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-
-# set up new lda model.
